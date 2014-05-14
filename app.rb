2b31b4b467bb359fa54de18617ee9c13ff5d8bea
Chadwick Flores <chadwickflores@gmail.com>
2014-05-13 19:43:53 -0500

    setup form, add error page, get, post

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 class IdeaBoxApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  not_found do
+    erb :error
+  end
+
   get '/' do
-    "Hello, World!"
+  	erb :index 
+  end
+
+  post '/' do
+   	# 1. Create an idea based on the form parameters
+ 		# 2. Store it
+  	# 3. Send us back to the index page to see all ideas
+  	"Creating an IDEA!"
 	end
 end
